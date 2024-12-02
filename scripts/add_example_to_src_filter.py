9715418f81d9363978dff8de4f488fd465d96149
Braidan <legohead259@gmail.com>
2024-12-01 22:34:51 -0500

    Migrate script to just add a properly converted ino CPP file to the build src path

--- scripts/add_example_to_src_filter.py
+++ scripts/add_example_to_src_filter.py
@@ -4,20 +4,12 @@ Import("env")
 
 custom_example = env.GetProjectOption("custom_example", None)
 
-print("Custom example is: " + str(custom_example))
-
 if custom_example is not None:
-    prev_src_dir = env["PROJECT_SRC_DIR"]
+    print("Custom example is: " + str(custom_example))
     proj_path = env["PROJECT_DIR"]
-    # paths are seen as relative from project root location
+    # Paths are seen as relative from project root location
     example_path = os.path.join(proj_path, custom_example)
-    # ConvertToInoCpp() only looks at .ino files from PROJECT_SRC_DIR
-    # we temporarily redirect this variable to the examples folder
-    env["PROJECT_SRC_DIR"] = example_path
-    print("Converting ino for " + example_path)
-    env.ConvertInoToCpp()
-    env["PROJECT_SRC_DIR"] = prev_src_dir
-    # also automatically add to build_src_filter
+    # Automatically add to build_src_filter
     print("CURRENT SOURCE FILTER")
     print(env.get("SRC_FILTER", ""))
     if "SRC_FILTER" in env:
@@ -28,7 +20,7 @@ if custom_example is not None:
             env["SRC_FILTER"] += " +<%s>" % example_path
     else:
         # not set previously:
-        env["SRC_FILTER"] = "+<*> +<%s>" % example_path
+        env["SRC_FILTER"] = "-<*> +<%s>" % example_path
     print("SOURCE FILTER NOW")
     print(env.get("SRC_FILTER", ""))
     # ify ou want to set data/ directory per-example for SPIFFS/LittleFS upload
